b868778ede16c0a1dd4fe3875dd047b7035e4678
mikemccomb <mikemccomb@gmail.com>
2023-03-27 14:26:52 -0500

    Adds new entry link on subject show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  root "categories#index"
+
   resources :entries
   resources :subjects
   resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
