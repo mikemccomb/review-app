49ce43c90fb8876e46d7b79cea9510225220c724
mikemccomb <mikemccomb@gmail.com>
2023-03-28 14:00:09 -0500

    Adds subject_name to entries model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_185530) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_185908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_185530) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subject_name"
   end
 
   create_table "subjects", force: :cascade do |t|
