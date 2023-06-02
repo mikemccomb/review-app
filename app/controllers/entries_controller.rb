f855b6b4fccce9bdbf25b12899d12626365746ac
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-06-02 15:29:05 -0500

    Resolves subject/entry layering

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -60,7 +60,7 @@ class EntriesController < ApplicationController
     @entry.destroy
 
     respond_to do |format|
-      format.html { redirect_to subject_entries_path(@subject), notice: "Entry was successfully destroyed." }
+      format.html { redirect_to (@entry.subject), notice: "Entry was successfully destroyed." }
       format.json { head :no_content }
     end
   end
